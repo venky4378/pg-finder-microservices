1786551513efb95365381c7e0e25397ed135944f
venky4378 <venkyswamy437@gmailcom>
2026-09-20 11:28:35 +0530

    feat: complete Eureka dynamic discovery, Gateway lb routing, OpenFeign loadbalancer, and realistic seed data

--- booking-service/src/main/java/com/pgfinder/booking_service/client/HostelClient.java
+++ booking-service/src/main/java/com/pgfinder/booking_service/client/HostelClient.java
@@ -4,10 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-        name = "hostel-service",
-        url = "${hostel.service.url}"
-)
+@FeignClient(name = "hostel-service")
 public interface HostelClient {
 
     @GetMapping("/api/v1/hostels/{id}")
@@ -17,8 +14,5 @@ public interface HostelClient {
     BedClientResponseDto getBedById(@PathVariable Long id);
 
     @GetMapping("/api/v1/beds/hostel/{hostelId}/bed/{bedId}")
-    BedClientResponseDto getBedByHostelId(
-            @PathVariable Long hostelId,
-            @PathVariable Long bedId
-    );
+    BedClientResponseDto getBedByHostelId(@PathVariable Long hostelId, @PathVariable Long bedId);
 }
--- booking-service/src/main/java/com/pgfinder/booking_service/client/UserClient.java
+++ booking-service/src/main/java/com/pgfinder/booking_service/client/UserClient.java
@@ -4,10 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-            name = "user-service",
-            url = "${user.service.url}"
-)
+@FeignClient(name = "user-service")
 public interface UserClient {
     @GetMapping("api/v1/users/{id}")
     UserClientResponseDto getUserById(@PathVariable Long id);
